d6fdcef030072f7721c4e8b165a7b2936bee6512
tan.nguyen <raucaixanh04@gmail.com>
2024-11-06 16:06:04 +0700

    Update color palette.

--- web/themes/custom/fstheme/tailwind.config.js
+++ web/themes/custom/fstheme/tailwind.config.js
@@ -18,10 +18,10 @@ export const theme = {
   },
   extend: {
     colors: {
-      'fs-red': '#a71f23',
-      'fs-red-tint': '#e5bcbd',
-      'fs-dark-grey': '#333',
-      'fs-light-grey': '#ddd',
+      'fs-red': '#851520',
+      'fs-red-tint': '#B11F24',
+      'fs-dark-grey': '#BFBFBF',
+      'fs-light-grey': '#F5F5F5',
     },
     boxShadow: {
       popped: '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
