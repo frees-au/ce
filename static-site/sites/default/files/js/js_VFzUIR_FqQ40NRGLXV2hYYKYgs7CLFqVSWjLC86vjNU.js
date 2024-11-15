ae4a596154a3c8e4f99b294817af506117f3f8cb
tan.nguyen <raucaixanh04@gmail.com>
2024-11-15 15:42:52 +0700

    Re-generate static site

---
+++ static-site/sites/default/files/js/js_VFzUIR_FqQ40NRGLXV2hYYKYgs7CLFqVSWjLC86vjNU.js
@@ -0,0 +1,2 @@
+/* @license GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+(function(){const settingsElement=document.querySelector('head > script[type="application/json"][data-drupal-selector="drupal-settings-json"], body > script[type="application/json"][data-drupal-selector="drupal-settings-json"]');window.drupalSettings={};if(settingsElement!==null)window.drupalSettings=JSON.parse(settingsElement.textContent);})();;
